e11f8c38e5abd5f7519792e9981cf79453d523cc
niki <you@example.com>
2023-10-21 18:13:53 +0800

    re-make

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Button from "./components/Buttons/Button";
+import Header from "./Header/Header";
+import {useEffect} from "react";
 
-
+const tg = window.Telegram.WebApp;
 
 function App() {
 
+  useEffect(() => {
+    tg.ready()
+  })
 
 
   return (
     <div className="App">
-
+      <Header></Header>
+      work
     </div>
   );
 }
