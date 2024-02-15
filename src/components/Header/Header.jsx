85f5301a20ccb93c2614e2a16f655e33c6c424f5
niki <you@example.com>
2024-02-15 18:21:53 +0800

    update form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,15 +10,19 @@ const Header = (props) => {
 
     return (
         <div className={'header'}>
-            <span className={'info'}>
-                Форма обратной связи
-            </span>
-            <span className={'username'}>
-                Добро пожаловать, {user?.username}
-            </span>
-            <span className={'info'}>
-                Укажите Ваше ФИО, специалиста к которому хотите обратиться, опишите сам вопрос и Email для обратной связи
-            </span>
+            <div>
+                <h2 className={'info'}>
+                    Форма обратной связи
+                </h2>
+            </div>
+
+
+            {/*<span className={'username'}>*/}
+            {/*    {user?.username}*/}
+            {/*</span>*/}
+            {/*<div className={'info'}>*/}
+            {/*    Укажите Ваше ФИО, специалиста к которому хотите обратиться, опишите сам вопрос и Email для обратной связи*/}
+            {/*</div>*/}
         </div>
     );
 };
